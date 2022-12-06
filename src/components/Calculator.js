7d34ab21d4e0ec03b13d2e5829ea7c50f57e10af
FuadNabi <fuad.nebiyev95@mail.ru>
2022-12-06 17:07:59 +0400

    Create project body and add styles to project

---
+++ src/components/Calculator.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="buttons-cont">
+        <div className="calc-section">0</div>
+        <div className="grid-container">
+          <div className="container-row">
+            <button type="button" className="button-grey">AC</button>
+            <button type="button" className="button-grey">+/-</button>
+            <button type="button" className="button-grey">%</button>
+            <button type="button" className="button-orange">÷</button>
+          </div>
+
+          <div className="container-row">
+            <button type="button" className="button-grey">7</button>
+            <button type="button" className="button-grey">8</button>
+            <button type="button" className="button-grey">9</button>
+            <button type="button" className="button-orange">x</button>
+          </div>
+
+          <div className="container-row">
+            <button type="button" className="button-grey">4</button>
+            <button type="button" className="button-grey">5</button>
+            <button type="button" className="button-grey">6</button>
+            <button type="button" className="button-orange">-</button>
+          </div>
+
+          <div className="container-row">
+            <button type="button" className="button-grey">1</button>
+            <button type="button" className="button-grey">2</button>
+            <button type="button" className="button-grey">3</button>
+            <button type="button" className="button-orange">+</button>
+          </div>
+
+          <div className="third-row-bottom">
+            <button type="button" className="button-grey">0</button>
+            <button type="button" className="button-grey">.</button>
+            <button type="button" className="button-orange">=</button>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
