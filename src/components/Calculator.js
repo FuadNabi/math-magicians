022847d984a3e27f0c13247b93aef33bebfcaea3
FuadNabi <fuad.nebiyev95@mail.ru>
2022-12-08 12:26:11 +0400

    Refactor the Calculator component from class based to funtion

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,61 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.buttonClick = this.buttonClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState();
 
-  buttonClick(element) {
-    this.setState((state) => calculate(state, element.target.textContent));
-  }
+  const buttonClick = (element) => {
+    setState(calculate(state, element.target.textContent));
+  };
 
-  render() {
-    const { next } = this.state;
-    const { total } = this.state;
+  const { next } = state;
+  const { total } = state;
 
-    return (
-      <div className="buttons-cont">
-        <div className="calc-section">{next || total || 0}</div>
-        <div className="grid-container">
-          <div className="container-row">
-            <button type="button" onClick={this.buttonClick} className="button-grey">AC</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">+/-</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">%</button>
-            <button type="button" onClick={this.buttonClick} className="button-orange">÷</button>
-          </div>
-
-          <div className="container-row">
-            <button type="button" onClick={this.buttonClick} className="button-grey">7</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">8</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">9</button>
-            <button type="button" onClick={this.buttonClick} className="button-orange">x</button>
-          </div>
-
-          <div className="container-row">
-            <button type="button" onClick={this.buttonClick} className="button-grey">4</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">5</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">6</button>
-            <button type="button" onClick={this.buttonClick} className="button-orange">-</button>
-          </div>
-
-          <div className="container-row">
-            <button type="button" onClick={this.buttonClick} className="button-grey">1</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">2</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">3</button>
-            <button type="button" onClick={this.buttonClick} className="button-orange">+</button>
-          </div>
-
-          <div className="third-row-bottom">
-            <button type="button" onClick={this.buttonClick} className="button-grey">0</button>
-            <button type="button" onClick={this.buttonClick} className="button-grey">.</button>
-            <button type="button" onClick={this.buttonClick} className="button-orange">=</button>
-          </div>
-        </div>
-
+  return (
+    <div className="buttons-cont">
+      <div className="calc-section">{next || total || 0}</div>
+      <div className="container-row">
+        <button type="button" onClick={buttonClick} className="button-grey">AC</button>
+        <button type="button" onClick={buttonClick} className="button-grey">+/-</button>
+        <button type="button" onClick={buttonClick} className="button-grey">%</button>
+        <button type="button" onClick={buttonClick} className="button-orange">÷</button>
       </div>
-    );
-  }
-}
+
+      <div className="container-row">
+        <button type="button" onClick={buttonClick} className="button-grey">7</button>
+        <button type="button" onClick={buttonClick} className="button-grey">8</button>
+        <button type="button" onClick={buttonClick} className="button-grey">9</button>
+        <button type="button" onClick={buttonClick} className="button-orange">x</button>
+      </div>
+
+      <div className="container-row">
+        <button type="button" onClick={buttonClick} className="button-grey">4</button>
+        <button type="button" onClick={buttonClick} className="button-grey">5</button>
+        <button type="button" onClick={buttonClick} className="button-grey">6</button>
+        <button type="button" onClick={buttonClick} className="button-orange">-</button>
+      </div>
+
+      <div className="container-row">
+        <button type="button" onClick={buttonClick} className="button-grey">1</button>
+        <button type="button" onClick={buttonClick} className="button-grey">2</button>
+        <button type="button" onClick={buttonClick} className="button-grey">3</button>
+        <button type="button" onClick={buttonClick} className="button-orange">+</button>
+      </div>
+
+      <div className="third-row-bottom">
+        <button type="button" onClick={buttonClick} className="button-grey">0</button>
+        <button type="button" onClick={buttonClick} className="button-grey">.</button>
+        <button type="button" onClick={buttonClick} className="button-orange">=</button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
